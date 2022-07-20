fd3fd5b3575a9773429bfa3b27a1d3b4bec29aba
Shani Sinojiya <shanisinojiya@gmail.com>
2022-07-20 13:57:29 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import Testimonials from "./components/Testimonials/Testimonials";
 function App() {
   return (
     <>
-    {/* https://youtu.be/o3IP5HeFRO0?t=2142 */}
+    {/* https://youtu.be/o3IP5HeFRO0?t=3988 */}
       <SideBar />
       <main className="main">
         <Home />
